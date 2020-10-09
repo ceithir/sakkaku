7862c26861e7bd325633a9c2e4033bda8f1d8250
Ceithir <ceathramh@gmail.com>
2020-10-09 14:29:54 +0200

    Add React SPA

---
+++ public/react/precache-manifest.df9f0d03b65f7ced5318b30955148c50.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69696bc731850c517aa81edbfc89eaa",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "85cbf5e28acef79f76b1",
+    "url": "/react/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "b92025877bd0b3f2b9b4",
+    "url": "/react/static/css/main.27a0204d.chunk.css"
+  },
+  {
+    "revision": "85cbf5e28acef79f76b1",
+    "url": "/react/static/js/2.cfaf5b78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react/static/js/2.cfaf5b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92025877bd0b3f2b9b4",
+    "url": "/react/static/js/main.7f9742b0.chunk.js"
+  },
+  {
+    "revision": "9042eeeb25b020189839",
+    "url": "/react/static/js/runtime-main.3da33eb3.js"
+  },
+  {
+    "revision": "f14fed519865099ad4ff1e021bfc1329",
+    "url": "/react/static/media/black.f14fed51.png"
+  },
+  {
+    "revision": "f0ca30761d07014d179859b723986919",
+    "url": "/react/static/media/blacket.f0ca3076.png"
+  },
+  {
+    "revision": "e44a1dd06fd5a95b238b8b97cf9dd04a",
+    "url": "/react/static/media/blacko.e44a1dd0.png"
+  },
+  {
+    "revision": "3a5ad7bb5f3f859e81d408134023f139",
+    "url": "/react/static/media/blackot.3a5ad7bb.png"
+  },
+  {
+    "revision": "216d45467500362909193892f2aa4efd",
+    "url": "/react/static/media/blacks.216d4546.png"
+  },
+  {
+    "revision": "3fad81954a87437863975d971c1a1cdf",
+    "url": "/react/static/media/blackst.3fad8195.png"
+  },
+  {
+    "revision": "c24c0c510f833e9e885f3e5410b6ae64",
+    "url": "/react/static/media/white.c24c0c51.png"
+  },
+  {
+    "revision": "7755ca7934ea80c93ac6f6430972e216",
+    "url": "/react/static/media/whitee.7755ca79.png"
+  },
+  {
+    "revision": "d10cd0cc4afe6474b679b9098b24b063",
+    "url": "/react/static/media/whiteet.d10cd0cc.png"
+  },
+  {
+    "revision": "1e81fee06f1ea814969e47c300566d0d",
+    "url": "/react/static/media/whiteo.1e81fee0.png"
+  },
+  {
+    "revision": "7cb9db03a6d4f798ea8c2bab809bff45",
+    "url": "/react/static/media/whites.7cb9db03.png"
+  },
+  {
+    "revision": "f76a057b0391ef409085f3a272405e6d",
+    "url": "/react/static/media/whiteso.f76a057b.png"
+  },
+  {
+    "revision": "05051e665e72fb4e75ec0a9a4c60529b",
+    "url": "/react/static/media/whitest.05051e66.png"
+  }
+]);
