1d7364383eb9143b4bf7967b448d56964ef610a5
Ceithir <ceathramh@gmail.com>
2020-10-11 21:23:01 +0200

    Add meh try/catch

--- app/Http/Controllers/Api/FFG/L5R/RollController.php
+++ app/Http/Controllers/Api/FFG/L5R/RollController.php
@@ -5,27 +5,45 @@ namespace App\Http\Controllers\Api\FFG\L5R;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Concepts\FFG\L5R\Roll;
+use Assert\Assertion;
+use Assert\InvalidArgumentException;
 
 class RollController extends Controller
 {
     public function create(Request $request)
     {
-      return response()->json(Roll::init($request->all()), 201);
+      return $this->handleRequest('create', $request);
     }
 
     public function keep(Request $request)
     {
-      $roll = Roll::fromArray($request->input('roll'));
-      $roll->keep($request->input('positions'));
-
-      return response()->json($roll);
+      return $this->handleRequest('keep', $request);
     }
 
     public function reroll(Request $request)
     {
-      $roll = Roll::fromArray($request->input('roll'));
-      $roll->reroll($request->input('positions'), $request->input('modifier'));
+      return $this->handleRequest('reroll', $request);
+    }
 
-      return response()->json($roll);
+    private function handleRequest($action, Request $request)
+    {
+      Assertion::inArray($action, ['create', 'keep', 'reroll']);
+      try {
+        if ($action === 'create') {
+          return response()->json(Roll::init($request->all()), 201);
+        }
+
+        $roll = Roll::fromArray($request->input('roll'));
+        if ($action === 'reroll') {
+          $roll->reroll($request->input('positions'), $request->input('modifier'));
+        }
+        if ($action === 'keep') {
+          $roll->keep($request->input('positions'));
+        }
+        return response()->json($roll);
+      } catch (InvalidArgumentException $e) {
+        report($e);
+        return response(null, 400);
+      }
     }
 }
