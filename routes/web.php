5697259a284e8b110b3bc3d73f046ce4e9508b6a
Ceithir <ceathramh@gmail.com>
2021-07-26 21:08:19 +0200

    Activate map route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/resources/rokugan-map', function () {
+    return File::get(public_path().'/react/index.html');
+});
+
 Route::get('/probabilities', function () {
     return File::get(public_path().'/react/index.html');
 });
