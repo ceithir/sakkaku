74800a15c04d115b7d1552450ee2afaad435be96
Ceithir <ceathramh@gmail.com>
2021-11-29 10:27:01 +0100

    Linter

--- app/Models/ContextualizedRoll.php
+++ app/Models/ContextualizedRoll.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
+use App\Concepts\FFG\L5R\InheritanceRoll as L5RHeritageRoll;
+use App\Concepts\FFG\L5R\Roll as L5RRoll;
+use App\Concepts\Roll;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
-use App\Concepts\Roll;
-use App\Concepts\FFG\L5R\Roll as L5RRoll;
-use App\Concepts\FFG\L5R\InheritanceRoll as L5RHeritageRoll;
 
 class ContextualizedRoll extends Model
 {
@@ -33,8 +32,10 @@ class ContextualizedRoll extends Model
         switch ($this->type) {
             case 'FFG-L5R':
                 return L5RRoll::fromArray($this->roll);
+
             case 'FFG-L5R-Heritage':
                 return L5RHeritageRoll::fromArray($this->roll);
+
             default:
                 throw 'Corrupted roll';
         }
