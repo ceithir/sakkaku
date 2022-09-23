c45b8565fce451af5a13085f3552f021f1359745
Ceithir <ceathramh@gmail.com>
2022-09-23 14:46:53 +0200

    Handle token backside

--- app/Http/Controllers/Api/Cards/DrawController.php
+++ app/Http/Controllers/Api/Cards/DrawController.php
@@ -7,8 +7,10 @@ use App\Concepts\Cards\Draw;
 use App\Http\Controllers\Controller;
 use App\Models\ContextualizedRoll;
 use App\Models\Deck;
+use Assert\Assertion;
 use Assert\InvalidArgumentException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class DrawController extends Controller
@@ -39,20 +41,46 @@ class DrawController extends Controller
             'description' => 'required|string',
             'parameters' => 'required|array',
             'metadata' => 'nullable|array',
+            'code' => 'nullable|string',
         ]);
 
         try {
-            $roll = new ContextualizedRoll();
-            $roll->type = self::ROLL_TYPE;
-            $roll->user_id = $request->user()->id;
-            $roll->campaign = $request->input('campaign');
-            $roll->character = $request->input('character');
-            $roll->description = $request->input('description');
-            $roll->setRoll(Draw::init(
-                $request->input('parameters'),
-                metadata: $request->input('metadata', [])
-            ));
-            $roll->result = $roll->getRoll()->result();
+            $roll = $this->prepareRoll($request);
+
+            if ((bool) $request->input('code')) {
+                $deck = Deck::where('uuid', $request->input('code'))->first();
+                Assertion::notNull($deck);
+
+                $currentDeckState = $deck->state['current'];
+                $draw = $roll->getRoll();
+                $supposedDeckState = $draw->parameters->deck->toArray();
+                sort($currentDeckState);
+                sort($supposedDeckState);
+                Assertion::eq(
+                    array_values($currentDeckState),
+                    array_values($supposedDeckState),
+                    'Inconsistent deck state, aborting to avoid bad surprise.'
+                );
+
+                DB::transaction(function () use ($roll, $deck) {
+                    $roll->save();
+
+                    $deckState = $deck->state;
+                    $hand = $roll->getRoll()->hand;
+                    $deckState['current'] = array_values(array_filter(
+                        $deckState['current'],
+                        function ($n) use ($hand) {
+                            return !in_array($n, $hand);
+                        }
+                    ));
+                    $deckState['draws'] = array_merge($deckState['draws'], [$roll->id]);
+                    $deck->state = $deckState;
+                    $deck->save();
+                });
+
+                return response()->json($this->toJson($roll));
+            }
+
             $roll->save();
 
             return response()->json($this->toJson($roll));
@@ -102,6 +130,23 @@ class DrawController extends Controller
     public function showDeck(string $uuid)
     {
         return response()->json(Deck::where('uuid', $uuid)->firstOrFail());
+    }
+
+    private function prepareRoll(Request $request): ContextualizedRoll
+    {
+        $roll = new ContextualizedRoll();
+        $roll->type = self::ROLL_TYPE;
+        $roll->user_id = $request->user()->id;
+        $roll->campaign = $request->input('campaign');
+        $roll->character = $request->input('character');
+        $roll->description = $request->input('description');
+        $roll->setRoll(Draw::init(
+            $request->input('parameters'),
+            metadata: $request->input('metadata', [])
+        ));
+        $roll->result = $roll->getRoll()->result();
+
+        return $roll;
     }
 
     private function toJson(ContextualizedRoll $roll): array
