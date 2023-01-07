575dfbbe9e8e301cc23bd9859645cc3f8fbf53d8
Ceithir <ceathramh@gmail.com>
2023-01-07 22:28:36 +0100

    Add TN
    Probably should be a metadata
    But simpler for validation to have it as a parameter

--- app/Concepts/Licensed/Cyberpunk/Parameters.php
+++ app/Concepts/Licensed/Cyberpunk/Parameters.php
@@ -8,11 +8,17 @@ class Parameters
 {
     public int $modifier;
 
+    public ?int $tn;
+
     public function __construct(array $parameters)
     {
         $modifier = $parameters['modifier'] ?? 0;
         Assertion::integer($modifier);
 
+        $tn = $parameters['tn'] ?? null;
+        Assertion::nullOrInteger($tn);
+
         $this->modifier = $modifier;
+        $this->tn = $tn;
     }
 }
