557ee623940553d3ca8aadc77a0b7946f226e72e
Ceithir <ceathramh@gmail.com>
2023-03-10 17:54:23 +0100

    Add new route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/gm/prefiller', function () {
+    return File::get(public_path().'/react/index.html');
+});
+
 Route::get('/cyberpunk/roll', function () {
     return File::get(public_path().'/react/index.html');
 });
