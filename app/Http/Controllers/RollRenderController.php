4c55c1f467583f4c70c1163abf5493a574a1d57b
Ceithir <ceathramh@gmail.com>
2023-10-25 10:33:14 +0200

    Slightly prettier separator

--- app/Http/Controllers/RollRenderController.php
+++ app/Http/Controllers/RollRenderController.php
@@ -11,7 +11,7 @@ class RollRenderController extends Controller
         $roll = ContextualizedRoll::where('type', 'AEG-L5R')->findOrFail($id);
 
         $r = $roll->getRoll();
-        $description = "{$roll->campaign} | {$roll->character} | ";
+        $description = "{$roll->campaign} – {$roll->character} – ";
         $description .= "{$r->parameters->roll}k{$r->parameters->keep} => {$r->result()['total']}";
         if ($r->parameters->tn) {
             $description .= ' '."(TN: {$r->parameters->tn})";
