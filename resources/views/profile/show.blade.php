ef38c106d286a254076bac5187bafaa6acbb755f
Ceithir <ceathramh@gmail.com>
2023-10-27 10:23:51 +0200

    Jetstream update
    https://github.com/laravel/jetstream/blob/4.x/UPGRADE.md
    
    I'm not sure half these files are even used
    But simpler to update them all as a batch

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,7 +10,7 @@
             @livewire('profile.update-profile-information-form')
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <x-jet-section-border />
+                <x-section-border />
             
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
@@ -18,20 +18,20 @@
             @endif
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <x-jet-section-border />
+                <x-section-border />
 
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.two-factor-authentication-form')
                 </div>
             @endif
 
-            <x-jet-section-border />
+            <x-section-border />
 
             <div class="mt-10 sm:mt-0">
                 @livewire('profile.logout-other-browser-sessions-form')
             </div>
 
-            <x-jet-section-border />
+            <x-section-border />
 
             <div class="mt-10 sm:mt-0">
                 @livewire('profile.delete-user-form')
