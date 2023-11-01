eb5b57143755974f305c7f7695d7aa5e43cd0c94
Ceithir <ceathramh@gmail.com>
2023-11-01 22:10:44 +0100

    Antd migration
    https://ant.design/docs/react/migration-v5
    
    Manually migrated the few Antd Less variables used
    Allowing to get rid of that dependency entirely

--- front/craco.config.js
+++ front/craco.config.js
@@ -1,17 +1,11 @@
 /**
- * https://github.com/DocSpring/craco-antd
  * https://github.com/DocSpring/craco-less/issues/45#issuecomment-692571756
  */
 
-const CracoAntDesignPlugin = require("craco-antd");
 const CracoLessPlugin = require("craco-less");
 
 module.exports = {
   plugins: [
-    // This plugin takes care of the .less files (and does a bit of magic on the side)
-    {
-      plugin: CracoAntDesignPlugin,
-    },
     // This plugin take scare of the .module.less files
     {
       plugin: CracoLessPlugin,
