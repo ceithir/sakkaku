670aae6c7a4e62795b275c197e02265933dac9ba
Ceithir <ceathramh@gmail.com>
2023-11-01 11:46:42 +0100

    Move data to static JSON file

--- front/src/features/d10/Odds.js
+++ front/src/features/d10/Odds.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import { Typography } from "antd";
 import { getProbability } from "./probabilities";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const { Text } = Typography;
 
+let cachedRawData;
+
+// TODO Minimal error handling
+const loadRawData = async (setRawData) => {
+  if (cachedRawData) {
+    setRawData(cachedRawData);
+    return;
+  }
+
+  const response = await fetch("/media/dice/AEG/L5R/probabilities.json");
+  const data = await response.json();
+  cachedRawData = data["raw"];
+  setRawData(cachedRawData);
+};
+
 const Odds = ({ roll, keep, tn, explosions = [10], rerolls = [] }) => {
+  const [rawData, setRawData] = useState();
+
+  useEffect(() => {
+    if (!rawData) {
+      loadRawData(setRawData);
+    }
+  }, [rawData]);
+
   if (
     rerolls.includes(2) ||
     rerolls.includes(3) ||
@@ -13,8 +38,12 @@ const Odds = ({ roll, keep, tn, explosions = [10], rerolls = [] }) => {
     return <Text disabled={true}>{`Unknown`}</Text>;
   }
 
+  if (!rawData) {
+    return <LoadingOutlined />;
+  }
+
   return (
-    <strong>{`${getProbability({
+    <strong>{`${getProbability(rawData, {
       roll,
       keep,
       tn,
