f387a0ee519c0f887506783d0e22feee4b9de8ff
Ceithir <ceathramh@gmail.com>
2023-11-06 16:17:29 +0100

    Update test to cover a non SPA route

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,19 +2,21 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/rolls');
 
         $response->assertStatus(200);
     }
