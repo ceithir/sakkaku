2c381dd7ccc671051831f457f774cd1759c2aaf9
Ceithir <ceathramh@gmail.com>
2024-04-05 18:09:59 +0200

    The end
    For real this time

--- front/src/features/navigation/Layout.js
+++ front/src/features/navigation/Layout.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Alert } from "antd";
 import styles from "./Layout.module.less";
 import Menu from "./Menu";
+import ExternalLink from "./ExternalLink";
 
 const { Header, Content, Footer } = Layout;
+
+const TheEnd = () => {
+  return (
+    <Alert
+      description={
+        <>
+          <p>
+            {`Sakkaku will be decommissioned on `}
+            <b>{`June 30th, 2024`}</b>
+            {`.`}
+          </p>
+          <p>
+            {`Alternatives: `}
+            <ExternalLink href="https://orokos.com/roll/">{`Orokos`}</ExternalLink>
+            {`, `}
+            <ExternalLink href="https://help.roll20.net/hc/en-us/articles/360037773133-Dice-Reference">{`Roll20`}</ExternalLink>
+            {`, `}
+            <ExternalLink href="https://discord.com/application-directory/search?q=dice">{`many Discord bots`}</ExternalLink>
+            {`…`}
+          </p>
+        </>
+      }
+      type="info"
+      showIcon
+      closable
+    />
+  );
+};
 
 const CustomLayout = ({ children }) => {
   return (
@@ -11,7 +40,10 @@ const CustomLayout = ({ children }) => {
       <Header>
         <Menu />
       </Header>
-      <Content>{children}</Content>
+      <Content>
+        <TheEnd />
+        <>{children}</>
+      </Content>
       <Footer className={styles.footer}>
         {`A dice roller with a public roll history, for easy use in play-by-post.`}
         <br />
